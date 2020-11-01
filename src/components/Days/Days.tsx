16edb8d0ad7eb31c0d1fd77db73bc25c5535d200
alexcloudstar <alexcloudstar@gmail.com>
2020-11-01 13:30:43 +0200

    reset master

--- src/components/Days/Days.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Days.css';
-import { DaysProps } from '../../types';
-
-const Days: React.FC<DaysProps> = ({ day }) => {
-  return <div className='Days'>{day}</div>;
-};
-
-export default Days;
